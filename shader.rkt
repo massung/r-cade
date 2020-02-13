e6250f062d05af36a3b5e2ccfd01de9cd1cf8f66
Jeffrey Massung <massung@gmail.com>
2020-02-13 12:09:58 -0600

    Improved CRT scanline shader.

--- shader.rkt
+++ shader.rkt
@@ -19,54 +19,75 @@ All rights reserved.
 
 #|
 
-This CRT-effect shader below was taken (and modified) from:
+The CRT-effect shaders below was taken (and modified) from
+the Z_PI shaders, created by Greg Hogan:
 
-  https://filthypants.blogspot.com/2011/05/more-emulator-pixel-shaders-crt-updated.html
+  Copyright (C) 2017 Greg Hogan (SoltanGris42)
 
 A hearty thanks goes out to the CRT shader emulation community
 for their hard work!!
 
 |#
 
-(define crt-fragment-shader
+(define lcd-shader
   (string-append "uniform sampler2D texture;"
                  "uniform vec2 textureSize;"
                  ""
-                 "vec3 to_focus(float p)"
+                 "void main()"
                  "{"
-                 "    p = mod(p + 3.0, 3.0);"
-                 "    "
-                 "    if (p >= 2.0)"
-                 "      return vec3(p - 2.0, 0.0, 3.0 - p);"
-                 "    else if (p >= 1.0)"
-                 "      return vec3(0.0, 2.0 - p, p - 1.0);"
-                 "    else"
-                 "      return vec3(1.0 - p, p, 0.0);"
-                 "}"
+                 "    vec2 invSize = 1.0 / textureSize.xy;"
+                 "    vec2 texCoordInPixels = gl_TexCoord[0] * textureSize;"
+                 "    vec2 centerCoord = floor(texCoordInPixels.xy) + vec2(0.5, 0.5);"
+                 "    vec2 distFromCenter = abs(centerCoord - texCoordInPixels);"
+                 ""
+                 "    float Y = max(distFromCenter.x, distFromCenter.y);"
+                 ""
+                 "    Y = Y * Y;"
+                 "    float YY = Y * Y;"
+                 "    float YYY = YY * Y;"
+                 ""
+                 "    float lineWeight = YY - 2.7 * YYY;"
+                 "    lineWeight = 1.0 - 14.0 * lineWeight;"
+                 ""
+                 "    vec3 color = texture2D(texture, invSize * centerCoord).rgb * lineWeight;"
+                 "    color *= 0.6 + 0.4 * color.rgb;"
+                 ""
+                 "    gl_FragColor = vec4(color.rgb, 1.0);"
+                 "}"))
+
+;; ----------------------------------------------------
+
+(define scanline-shader
+  (string-append "uniform sampler2D texture;"
+                 "uniform vec2 textureSize;"
                  ""
                  "void main()"
                  "{"
-                 "    float y = mod(gl_TexCoord[0].y * textureSize.y, 1.0);"
-                 "    float intensity = exp(-0.2 * y);"
+                 "    float whichMask = fract(gl_FragCoord.x * 0.5);"
+                 "    float mask = 1.0 + float(whichMask < 0.5) * -0.3;"
                  ""
-                 "    vec2 one_x = vec2(1.0 / (3.0 * textureSize.x), 0.0);"
+                 "    vec2 invSize = 1.0 / textureSize.xy;"
+                 "    vec2 texCoordInPixels = gl_TexCoord[0] * textureSize;"
+                 "    vec2 centerCoord = floor(texCoordInPixels.xy) + vec2(0.5, 0.5);"
+                 "    vec2 distFromCenter = abs(centerCoord - texCoordInPixels);"
                  ""
-                 "    vec3 color = texture2D(texture, gl_TexCoord[0].xy - 0.0 * one_x).rgb;"
-                 "    vec3 color_p = texture2D(texture, gl_TexCoord[0].xy - 1.0 * one_x).rgb;"
-                 "    vec3 color_pp = texture2D(texture, gl_TexCoord[0].xy - 2.0 * one_x).rgb;"
+                 "    float Y = distFromCenter.y * distFromCenter.y;"
+                 "    float YY = Y * Y;"
+                 "    float scanLineWeight = 1.2 - 6.0 * (Y - 2.05 * YY);"
+                 "    float scanLineWeightB = 1.0 - 14.0 * (YY - 2.8 * YY * Y);"
                  ""
-                 "    float x = 3.0 * gl_TexCoord[0].x * textureSize.x;"
+                 "    float tx = invSize.x * (centerCoord.x - 0.4 * distFromCenter.x);"
+                 "    float ty = invSize.y * (centerCoord.y - 0.3 * distFromCenter.y);"
                  ""
-                 "    vec3 focus = to_focus(x - 0.0);"
-                 "    vec3 focus_p = to_focus(x - 1.0);"
-                 "    vec3 focus_pp = to_focus(x - 2.0);"
+                 "    vec2 tc = vec2(tx, ty);"
+                 "    vec3 color = texture2D(texture, tc).rgb;"
                  ""
-                 "    vec3 result = "
-                 "      0.8 * color * focus + "
-                 "      0.6 * color_p * focus_p +"
-                 "      0.3 * color_pp * focus_pp;"
+                 "    color.rgb *= 0.8 + 0.2 * color.rgb;"
+                 "    color.rgb *= mix(scanLineWeight * mask, scanLineWeightB, dot(color.rgb, vec3(0.28)));"
                  ""
-                 "    result = 2.3 * pow(result, vec3(1.4));"
-                 ""
-                 "    gl_FragColor = vec4(intensity * result, 1.0);"
+                 "    gl_FragColor = vec4(color.rgb, 1.0);"
                  "}"))
+
+;; ----------------------------------------------------
+
+(define crt-fragment-shader scanline-shader)
