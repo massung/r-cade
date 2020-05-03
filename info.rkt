4fa34872c747e34252bfda9fedbaed13b73434c3
Jeffrey Massung <massung@gmail.com>
2020-05-02 20:26:55 -0500

    Updated version.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "r-cade")
-(define version "0.3.2")
+(define version "0.3.3")
 (define pkg-authors '("massung@gmail.com"))
 (define pkg-desc "R-cade game engine")
 (define deps '("base" "csfml"))
